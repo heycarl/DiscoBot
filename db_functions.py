c432423ec90698ac2db6276c608ee24c46e0e3d8
Alexandr <efimchiksasha@gmail.com>
2018-11-19 19:08:16 +0300

    SQLite integration added.

--- db_functions.py
+++ db_functions.py
@@ -1,7 +1,21 @@
-import sqlite3 as lite
-import sys
-
-con = lite.connect('disco.db')
-cur = con.cursor()
-cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
-cur.execute("INSERT INTO Cars VALUES(1, 'Audi', 52642)")
+import sqlite3
+connection = sqlite3.connect('disco.db', check_same_thread=False)
+cursor = connection.cursor()
+def create_db():
+    try:
+        cursor.execute("CREATE TABLE Users(Code INT, Name TEXT, Status INT)")
+        print("DB created")
+    except sqlite3.OperationalError:
+        print("DB is already created")
+def write_user_code(code):
+    cursor.execute(f"insert into Users values ({code}, 'Sasha', 1) ")
+    connection.commit()
+def check_user_code(code):
+    cursor.execute("SELECT Code FROM Users")
+    row = cursor.fetchall()
+    for item in row:
+        if item[0] == code:
+            return 1
+        else:
+            pass
+    return 0
--- other_functions.py
+++ other_functions.py
@@ -1,6 +1,17 @@
 from random import randint
+import db_functions
 
 def generate_code():
-    range_start = 10**(4-1)
-    range_end = (10**4)-1
+    db_functions.create_db()
+    while True:
+        num = generate_numbers()
+        if db_functions.check_user_code(num) == 0:
+            break
+        else:
+            pass
+    db_functions.write_user_code(num)
+    return num
+def generate_numbers():
+    range_start = 10 ** (4 - 1)
+    range_end = (10 ** 4) - 1
     return randint(range_start, range_end)
